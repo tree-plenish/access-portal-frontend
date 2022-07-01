ec18c2b5dade690d6c1513cd39123b31b9953045
allisonkwan <akwan2020@gmail.com>
2022-06-30 17:18:11 -0700

    Update tree requests excel button

--- src/ExportTreeRequestsButton.js
+++ src/ExportTreeRequestsButton.js
@@ -14,27 +14,12 @@ const ExportTreeRequestsButton = (props) => {
   let apiU = 'admin';
   let apiP = 'preeTlenish1#';
 
-  // new excel vars
-  const [uniqueSpecies, setUniqueSpecies] = useState(["Place", "Holder", "Species"]);
-  const [custNames, setCustNames] = useState([]);
-  const [specOne, setSpecOne] = useState([]);
-  const [specTwo, setSpecTwo] = useState([]);
-  const [specThree, setSpecThree] = useState([]);
-  const [submitTime, setSubmitTime] = useState([]);
-  const [address, setAddress] = useState([]);
-  const [zip, setZip] = useState([]);
-  const [email, setEmail] = useState([]);
-  const [phone, setPhone] = useState([]);
-  const [pickup, setPickup] = useState([]);
   const [finalArr, setFinalArr] = useState([]);
-
-  // refund table vars
-  const [refTable, setRefTable] = useState();
-  const [refProps, setRefProps] = useState([
-    { name: "Place", email: "iFeelGodInThisChilis@tn", phone: "123-456-7890", price_refunded: "50" },
-    { name: "Holder", email: "catchYou@theFlipityFlip", phone: "123-456-7890", price_refunded: "80" }
+  const [headers1, setHeaders1] = useState([ // placeholder values
+    { label: "Team ID", key: "id" },
+    { label: "Name", key: "name" },
+    { label: "Email or Phone", key: "email" },
   ]);
-  let refArr = [];
 
   function toTitleCase(str) { // function to capitalize first letter of each word; e.g. 'still woozy' becomes 'Still Woozy'
     let text = str.toLowerCase()
@@ -44,106 +29,19 @@ const ExportTreeRequestsButton = (props) => {
     return text;
   }
 
-  function traverseThreeSpecies(objName, schoolidnum) {
+  function traverseTreeOrders(objName) {
+    let headersTemp = [];
+    let keys = Object.keys(objName[0]);
+    console.log(keys)
+    for (let i = 0; i < keys.length; i++) {
+      headersTemp.push({ label: keys[i], key: keys[i] });
+    }
+    setHeaders1(headersTemp);
+    let ordersTemp = [];
     for (const prop in objName) {
-      let threeSpeciesTemp = ["Place", "Holder", "Species"];
-      threeSpeciesTemp[0] = objName[prop]['species_one'];
-      threeSpeciesTemp[1] = objName[prop]['species_two'];
-      threeSpeciesTemp[2] = objName[prop]['species_three'];
-      setUniqueSpecies(threeSpeciesTemp);
+      ordersTemp.push(objName[prop]);
     }
-  }
-
-  function traverseTreeOrders(objName, schoolidnum) {
-    let custNamesTemp = [];
-    let specOneTemp = [];
-    let specTwoTemp = [];
-    let specThreeTemp = [];
-    let submitTimeTemp = [];
-    let addressTemp = [];
-    let emailTemp = [];
-    let zipTemp = [];
-    let phoneTemp = [];
-    let pickupTemp = [];
-    for (const prop in objName) {
-      custNamesTemp.push(objName[prop]['cust_name']);
-    }
-    custNamesTemp = Array.from(new Set(custNamesTemp));
-    for (let i = 0; i < custNamesTemp.length; i++) {
-      let person = custNamesTemp[i];
-      let specOnePushed = false;
-      let specTwoPushed = false;
-      let specThreePushed = false;
-      for (const prop in objName) {
-        if (person == objName[prop]['cust_name']) {
-          if (objName[prop]['name'] == uniqueSpecies[0]) {
-            specOneTemp.push(objName[prop]['number']);
-            specOnePushed = true;
-          } else if (objName[prop]['name'] == uniqueSpecies[1]) {
-            specTwoTemp.push(objName[prop]['number']);
-            specTwoPushed = true;
-          } else if (objName[prop]['name'] == uniqueSpecies[2]) {
-            specThreeTemp.push(objName[prop]['number']);
-            specThreePushed = true;
-          }
-          submitTimeTemp[i] = objName[prop]['submit_time'];
-          addressTemp[i] = toTitleCase(objName[prop]['address']);
-          emailTemp[i] = objName[prop]['cust_email'];
-          zipTemp[i] = objName[prop]['cust_zipcode'];
-          phoneTemp[i] = objName[prop]['cust_phone_num'];
-          if (objName[prop]['pickup']) {
-            pickupTemp[i] = "Yes";
-          } else {
-            pickupTemp[i] = "No";
-          }
-        }
-      }
-      if (!specOnePushed) {
-        specOneTemp.push(0);
-      }
-      if (!specTwoPushed) {
-        specTwoTemp.push(0);
-      }
-      if (!specThreePushed) {
-        specThreeTemp.push(0);
-      }
-    }
-    setCustNames(custNamesTemp);
-    setSpecOne(specOneTemp);
-    setSpecTwo(specTwoTemp);
-    setSpecThree(specThreeTemp);
-    setSubmitTime(submitTimeTemp);
-    setAddress(addressTemp);
-    setEmail(emailTemp);
-    setPhone(phoneTemp);
-    setZip(zipTemp);
-    setPickup(pickupTemp);
-    let finalArrTemp = [];
-    finalArrTemp[0] = ["Customer Name"].concat(uniqueSpecies, ["Address"], ["Zip Code"], ["Pickup"], ["Email"], ["Phone Number"], ["Submit Date"]);
-    for (let i = 0; i < custNamesTemp.length; i++) {
-      finalArrTemp[i + 1] = [custNamesTemp[i], specOneTemp[i], specTwoTemp[i], specThreeTemp[i], addressTemp[i], zipTemp[i], pickupTemp[i], emailTemp[i], phoneTemp[i], new Date(submitTimeTemp[i] * 1000).toString()];
-    }
-    setFinalArr(finalArrTemp);
-  }
-
-  function traverseRefunds(objName) {
-    for (const prop in objName) {
-      refArr.push(objName[prop]);
-    }
-    setRefProps(refArr);
-    setRefTable(refArr.map(renderRefunds)); // method to make sure the table renders with updated data
-    return refArr;
-  }
-
-  const renderRefunds = (refund, index) => {
-    return (
-      <tr key={{ index }}>
-        <td>{refund.name}</td>
-        <td>{refund.email}</td>
-        <td>{refund.phone}</td>
-        <td>{refund.num_trees}</td>
-      </tr>
-    )
+    setFinalArr(ordersTemp);
   }
 
   // new function for excel tree species vars
@@ -157,7 +55,6 @@ const ExportTreeRequestsButton = (props) => {
       })
         .then(res => res.json())
         .then(data => {
-          traverseThreeSpecies(JSON.parse(data.threespecies));
           traverseTreeOrders(JSON.parse(data.order));
         });
     });
@@ -165,7 +62,7 @@ const ExportTreeRequestsButton = (props) => {
 
   useEffect(() => {
     getTreeOrders(props.user);
-  }, [uniqueSpecies, finalArr]);
+  }, [finalArr]);
 
   return (
     <div>
@@ -175,6 +72,7 @@ const ExportTreeRequestsButton = (props) => {
           size="lg"
           onClick={clickLink}>Download as Excel Sheet</Button>
         <CSVLink
+          headers={headers1}
           data={finalArr}
           filename='tree-plenish-tree-requests.csv'
           className='hidden'
@@ -182,23 +80,6 @@ const ExportTreeRequestsButton = (props) => {
           target='_blank'
         />
       </Container>
-      {/* <Container className="center">
-              <p className="col-title-text">Tree Refunds</p>
-              <p>Refunds are not accounted for in the above tree orders. Manually subtract the refunds.</p>
-              <ReactBootStrap.Table className="table">
-                  <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Trees Refunded</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                    {refTable}
-                  </tbody>
-                </ReactBootStrap.Table>
-            </Container> */}
     </div>
   )
 }
