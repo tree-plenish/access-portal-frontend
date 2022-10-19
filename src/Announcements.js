c08f12259e93e64598d32b69d953aa1746aac7cd
allisonkwan <akwan2020@gmail.com>
2022-10-19 12:29:54 -0400

    Add Calendly link

--- src/Announcements.js
+++ src/Announcements.js
@@ -4,6 +4,8 @@ import './App.css';
 const Announcements = () => {
     let thinkificLink = <a className="link-anim in-line" href="https://tree-plenish-event-planning.thinkific.com/">Thinkific.</a>
     let zoomLink = <a className="link-anim in-line" href="https://us06web.zoom.us/j/88064676662?pwd=dURMRytKcjB5WHNuTmN1bUt1aS9YQT09">Zoom link.</a>
+    let calendlyLink = <a className="link-anim in-line" href="https://calendly.com/treeplenish/sponsorships-meeting?month=2022-10">Calendly link.</a>
+
     return (
         <div>
             <div className="annOut">
@@ -17,6 +19,7 @@ const Announcements = () => {
                         </div>
                         <div className="annBox">
                             <h2>Reminders</h2>
+                            <p>Please sign up for a Sponsorship Meeting using this {calendlyLink} There are 2 meetings in October and 2 in November.</p>
                             <p>We have a mandatory meeting with all schools on December 1st at 8 pm ET.</p>
                             <p>Refer other schools to get $50 in free saplings for your event!</p>
                         </div>
