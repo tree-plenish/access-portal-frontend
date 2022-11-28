37caf0ae648577d9a008b400f49b04673de7e936
allisonkwan <akwan2020@gmail.com>
2022-11-28 12:45:01 -0500

    Add equity fund application link

--- src/Announcements.js
+++ src/Announcements.js
@@ -6,6 +6,7 @@ const Announcements = () => {
     let zoomLink = <a className="link-anim in-line" href="https://us06web.zoom.us/j/88064676662?pwd=dURMRytKcjB5WHNuTmN1bUt1aS9YQT09">Zoom link.</a>
     let calendlyLink = <a className="link-anim in-line" href="https://calendly.com/treeplenish/sponsorships-meeting?month=2022-10">Calendly link.</a>
     let referralLink = <a className="link-anim in-line" href="https://treeplenish.typeform.com/to/N7CHjsL4?typeform-source=admin.typeform.com">link</a>
+    let fundApplicationLink = <a className="link-anim in-line" href="https://treeplenish.typeform.com/to/TBWIfruH">link.</a>
 
     return (
         <div>
@@ -20,6 +21,7 @@ const Announcements = () => {
                         </div>
                         <div className="annBox">
                             <h2>Reminders</h2>
+                            <p>The Environmental Equity Fund application is now open! Apply by December 12th using this {fundApplicationLink}</p>
                             <p>Please sign up for a Sponsorship Meeting using this {calendlyLink} There are 2 meetings in October and 2 in November.</p>
                             <p>We have a mandatory meeting with all schools on December 1st at 8 pm ET.</p>
                             <p>Refer other schools to get $50 in free saplings for your event using this {referralLink} to our referral program!</p>
