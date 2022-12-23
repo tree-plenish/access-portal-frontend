90f2e1847dceebcc0a46769705977d8ea4db81c1
allisonkwan <akwan2020@gmail.com>
2022-12-23 09:28:32 -0700

    Update Reminders content

--- src/Announcements.js
+++ src/Announcements.js
@@ -4,9 +4,7 @@ import './App.css';
 const Announcements = () => {
     let thinkificLink = <a className="link-anim in-line" href="https://tree-plenish-event-planning.thinkific.com/">Thinkific.</a>
     let zoomLink = <a className="link-anim in-line" href="https://us06web.zoom.us/j/88064676662?pwd=dURMRytKcjB5WHNuTmN1bUt1aS9YQT09">Zoom link.</a>
-    let calendlyLink = <a className="link-anim in-line" href="https://calendly.com/treeplenish/sponsorships-meeting?month=2022-10">Calendly link.</a>
-    let referralLink = <a className="link-anim in-line" href="https://treeplenish.typeform.com/to/N7CHjsL4?typeform-source=admin.typeform.com">link</a>
-    let fundApplicationLink = <a className="link-anim in-line" href="https://treeplenish.typeform.com/to/TBWIfruH">link.</a>
+    let internApplicationLink = <a className="link-anim in-line" href="https://forms.gle/f5yTNUMnsvMna6uh6">Applications</a>
 
     return (
         <div>
@@ -21,10 +19,8 @@ const Announcements = () => {
                         </div>
                         <div className="annBox">
                             <h2>Reminders</h2>
-                            <p>The Environmental Equity Fund application is now open! Apply by December 12th using this {fundApplicationLink}</p>
-                            <p>Please sign up for a Sponsorship Meeting using this {calendlyLink} There are 2 meetings in October and 2 in November.</p>
-                            <p>We have a mandatory meeting with all schools on December 1st at 8 pm ET.</p>
-                            <p>Refer other schools to get $50 in free saplings for your event using this {referralLink} to our referral program!</p>
+                            <p>Want to be part of the Tree-Plenish organization team? Apply to be a an intern on our team this spring! {internApplicationLink} close January 4th.</p>
+                            <p>Download the Tree-Plenish app for easy access to your To-Do list, Event Statistics, Thinkific, and User Dashboard. We will also send important updates through push notifications. Check Thinkific for a quick download link or wherever you buy apps.</p>
                         </div>
                     </div>
                 </div>
